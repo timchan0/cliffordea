4de281a176f49e321fe57b2c910fa70ffd9677eb
Tim Chan <82181640+timchan0@users.noreply.github.com>
2025-07-07 10:47:52 +0100

    Improve Stim circuit manipulation
    * Allow specify which timeslices to uniformly depolarize
    * Add full Stim circuit for Distance-3 Double Cat Check using 6 Ancillas

--- cliffordep/circuits.py
+++ cliffordep/circuits.py
@@ -239,3 +239,137 @@ DETECTOR(0, 1, 1) rec[-1]
     """Adapted from the Stim file in `make_chunk_d3_double_cat_check()`
     in `code/src/cultiv/_construction/_cultivation_stage.py`.
     """
+
+    CIRCUIT = stim.Circuit(
+"""
+QUBIT_COORDS(0, 0) 0
+QUBIT_COORDS(0, 1) 1
+QUBIT_COORDS(1, 0) 2
+QUBIT_COORDS(1, 1) 3
+QUBIT_COORDS(2, 0) 4
+QUBIT_COORDS(2, 1) 5
+QUBIT_COORDS(2, 2) 6
+QUBIT_COORDS(2, 3) 7
+QUBIT_COORDS(3, 0) 8
+QUBIT_COORDS(3, 1) 9
+QUBIT_COORDS(3, 2) 10
+QUBIT_COORDS(4, 0) 11
+QUBIT_COORDS(4, 1) 12
+#!pragma POLYGON(0,0,1,0.25) 8 11 10 5
+#!pragma POLYGON(0,1,0,0.25) 7 10 5 3
+#!pragma POLYGON(1,0,0,0.25) 3 5 8 0
+TICK
+MPP Y0*Y8*Y11*Y10*Y7*Y5*Y3
+TICK
+MPP X8*X0*X3*X5
+TICK
+MPP X3*X5*X10*X7
+TICK
+MPP X11*X8*X5*X10
+TICK
+MPP Z8*Z0*Z3*Z5
+TICK
+MPP Z7*Z10*Z5*Z3
+TICK
+MPP Z11*Z8*Z5*Z10
+TICK
+RX 12 9 4 2 6 1
+TICK
+# CZ sweep[7] 0 sweep[7] 3 sweep[7] 5 sweep[7] 8
+# CX sweep[8] 0 sweep[8] 3 sweep[8] 5 sweep[8] 8
+# CZ sweep[9] 3 sweep[9] 5 sweep[9] 7 sweep[9] 10
+# CX sweep[10] 3 sweep[10] 5 sweep[10] 7 sweep[10] 10
+# CZ sweep[11] 5 sweep[11] 8 sweep[11] 10 sweep[11] 11
+# CX sweep[12] 5 sweep[12] 8 sweep[12] 10 sweep[12] 11
+S_DAG 0 3 5 7 8 10 11
+# CZ sweep[0] 0
+# CZ sweep[1] 3
+# CZ sweep[2] 5
+# CZ sweep[3] 7
+# CZ sweep[4] 8
+# CZ sweep[5] 10
+# CZ sweep[6] 11
+TICK
+CX 1 0 9 8 6 7 4 5 2 3 12 11
+TICK
+CX 3 1 5 6 9 12
+TICK
+CX 5 3 9 10
+TICK
+CX 5 9
+TICK
+# CZ sweep[0] 5
+# CZ sweep[1] 5
+# CZ sweep[2] 5
+# CZ sweep[3] 5
+# CZ sweep[4] 5
+# CZ sweep[5] 5
+# CZ sweep[6] 5
+MX 5
+DETECTOR(0, 0, 0) rec[-1] rec[-8]
+TICK
+RX 5
+# CZ sweep[0] 5
+# CZ sweep[1] 5
+# CZ sweep[2] 5
+# CZ sweep[3] 5
+# CZ sweep[4] 5
+# CZ sweep[5] 5
+# CZ sweep[6] 5
+TICK
+CX 5 9
+TICK
+CX 5 3 9 10
+TICK
+CX 3 1 5 6 9 12
+TICK
+CX 1 0 9 8 6 7 12 11 4 5 2 3
+TICK
+# CZ sweep[0] 0
+# CZ sweep[1] 3
+# CZ sweep[2] 5
+# CZ sweep[3] 7
+# CZ sweep[4] 8
+# CZ sweep[5] 10
+# CZ sweep[6] 11
+# CZ sweep[13] 0 sweep[13] 3 sweep[13] 5 sweep[13] 8
+# CX sweep[14] 0 sweep[14] 3 sweep[14] 5 sweep[14] 8
+# CZ sweep[15] 3 sweep[15] 5 sweep[15] 7 sweep[15] 10
+# CX sweep[16] 3 sweep[16] 5 sweep[16] 7 sweep[16] 10
+# CZ sweep[17] 5 sweep[17] 8 sweep[17] 10 sweep[17] 11
+# CX sweep[18] 5 sweep[18] 8 sweep[18] 10 sweep[18] 11
+S 5 10 11 8 7 3 0
+TICK
+MX 12 9 4 2 6 1
+DETECTOR(4, 1, 1) rec[-6]
+DETECTOR(3, 1, 1) rec[-5]
+DETECTOR(2, 1, 1) rec[-4] rec[-7]
+DETECTOR(1, 0, 1) rec[-3]
+DETECTOR(2, 2, 1) rec[-2]
+DETECTOR(0, 1, 1) rec[-1]
+TICK
+MPP X8*X0*X3*X5
+DETECTOR(3, 0, 2) rec[-1] rec[-8] rec[-14]
+TICK
+MPP X7*X10*X5*X3
+DETECTOR(1, 1, 3) rec[-1] rec[-9] rec[-14]
+TICK
+MPP X11*X8*X5*X10
+DETECTOR(4, 0, 4) rec[-1] rec[-10] rec[-14]
+TICK
+MPP Z8*Z0*Z3*Z5
+DETECTOR(3, 0, 5) rec[-1] rec[-14]
+TICK
+MPP Z7*Z10*Z5*Z3
+DETECTOR(2, 3, 6) rec[-1] rec[-14]
+TICK
+MPP Z11*Z8*Z5*Z10
+DETECTOR(4, 0, 7) rec[-1] rec[-14]
+TICK
+MPP Y0*Y8*Y11*Y10*Y7*Y5*Y3
+OBSERVABLE_INCLUDE(0) rec[-1] rec[-8] rec[-9] rec[-12] rec[-13]
+"""
+    )
+    """Copied from the Stim file in `make_chunk_d3_double_cat_check()`
+    in `code/src/cultiv/_construction/_cultivation_stage.py`.
+    """
--- cliffordep/noise/main.py
+++ cliffordep/noise/main.py
@@ -1,9 +1,16 @@
+from collections.abc import Container
+
 import stim
 
 from cliffordep.noise._noise import NoiseModel
+from cliffordep.noiseless_circuit_tools import split_by_ticks, compose_slices
 
 
-def uniformly_depolarize(circuit_without_noise: stim.Circuit, noise_level: float):
+def uniformly_depolarize(
+        circuit_without_noise: stim.Circuit,
+        noise_level: float,
+        noisy_timeslices: None | Container[int] = None,
+):
     """Near-standard circuit depolarizing noise.
 
     Everything has the same parameter p.
@@ -15,6 +22,12 @@ def uniformly_depolarize(circuit_without_noise: stim.Circuit, noise_level: float
     the input qubit. The input qubit is depolarized.
 
     Only the qubits that are acted upon at least once are subject to depolarization.
+
+    Input:
+    * `circuit_without_noise` a stim.Circuit without noise.
+    * `noise_level` a float in [0, 1].
+    * `noisy_timeslices` an optional iterable of timeslice indices to apply noise to.
+    If unspecified, noise is applied to all timeslices.
     """
     system_qubit_indices: set[int] = set()
     for instruction in circuit_without_noise:
@@ -22,7 +35,18 @@ def uniformly_depolarize(circuit_without_noise: stim.Circuit, noise_level: float
             for target in instruction.targets_copy():
                 if (val:=target.qubit_value) is not None:
                     system_qubit_indices.add(val)
-    return NoiseModel.uniform_depolarizing(noise_level).noisy_circuit(
-        circuit_without_noise,
-        system_qubit_indices=system_qubit_indices,
-    )
+    model = NoiseModel.uniform_depolarizing(noise_level)
+    if noisy_timeslices is None:
+        return model.noisy_circuit(
+            circuit_without_noise,
+            system_qubit_indices=system_qubit_indices,
+        )
+    layers = split_by_ticks(circuit_without_noise)
+    noisy_layers: list[stim.Circuit] = []
+    for timeslice, layer in enumerate(layers):
+        noisy_layer = model.noisy_circuit(
+                    layer,
+                    system_qubit_indices=system_qubit_indices,
+                ) if timeslice in noisy_timeslices else layer
+        noisy_layers.append(noisy_layer)
+    return compose_slices(noisy_layers)
