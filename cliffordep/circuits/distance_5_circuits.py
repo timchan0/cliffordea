7e7417f9f9fc69faff5b1158e4691ccd1286eb4d
Tim Chan <82181640+timchan0@users.noreply.github.com>
2026-08-24 20:45:31 +0800

    Find a working distance-5 double check
    * Add three more flag qubits to `D5A19F15`.
    * This results in a double check of fault distance 5.

--- cliffordep/circuits/distance_5_circuits.py
+++ cliffordep/circuits/distance_5_circuits.py
@@ -86,13 +86,41 @@ S_DAG 3 10 12 14 26 24 28 35 37
 
 
 class D5A19F15(Distance5DoubleCheck):
-    """The distance-5 double-check circuit using 19 ancillas and 15 flags."""
+    """The distance-5 double-check circuit using 19 ancillas and 15 flags.
+    
+    This circuit has fault distance 3.
+    The way this was designed was as follows:
+    once the X parity to be measured is positioned into a spanning tree,
+    we measure all but three pairwise Z parities between
+    neighboring qubits in the spanning tree.
+    """
     ANCILLA_INDICES = tuple(sorted((31, 40, 3, 6, 8, 10, 23, 25, 49, 1, 15, 21, 47, 42, 17, 33, 35, 37, 52)))
     FLAG_INDICES = tuple(sorted((2, 5, 11, 19, 27, 38, 44, 50, 39, 45, 28, 29, 30, 12, 13)))
     LOGICAL_S = stim.Circuit(
 """
 S 20 22 24 26 41 43 7 9 0 51
 S_DAG 4 14 32 16 18 36 34 48 46
+"""
+    )
+
+
+class D5A19F18(Distance5DoubleCheck):
+    """The distance-5 double-check circuit using 19 ancillas and 18 flags.
+    
+    This circuit has fault distance 5.
+    The way this was designed was as follows:
+    once the X parity to be measured is positioned into a spanning tree,
+    we measure all pairwise Z parities between
+    neighboring qubits in the spanning tree.
+    The three delayed Z parity measurements are necessary for fault distance 5;
+    without them we have `D5A19F15` of fault distance 3.
+    """
+    ANCILLA_INDICES = tuple(sorted((34, 43, 3, 6, 8, 10, 24, 26, 52, 1, 16, 22, 50, 45, 18, 36, 38, 40, 55)))
+    FLAG_INDICES = tuple(sorted((2, 5, 11, 20, 28, 41, 47, 53, 42, 48, 31, 32, 33, 13, 14, 12, 29, 30)))
+    LOGICAL_S = stim.Circuit(
+"""
+S 21 23 25 27 44 46 7 9 0 54
+S_DAG 4 15 35 17 19 39 37 51 49
 """
     )
 
